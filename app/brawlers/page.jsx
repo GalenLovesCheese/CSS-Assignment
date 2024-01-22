ab37d7ae2c3763bbebd8c36ae4b3e0500673e395
Kilo <freddythr1247@gmail.com>
2024-01-22 11:03:50 +0800

    Maps & Events

---
+++ app/brawlers/page.jsx
@@ -0,0 +1,34 @@
+/* page.jsx */
+
+"use client";
+
+import React from "react";
+import useSWR from "swr";
+import styles from "./page.module.css";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+export default function App1() {
+  const IP_API_URL = "https://api.brawlapi.com/v1/brawlers";
+  const { data, error, isLoading } = useSWR(IP_API_URL, fetcher);
+
+  if (error) {
+    return <h1>failed to load</h1>;
+  }
+  if (isLoading) {
+    return <h1 className="loading">loading...</h1>;
+  }
+
+  return (
+    <div className={`${styles.Container}`}>
+      <div className={`${styles.brawlers_container}`}>
+        {data.list.map((item, i) => (
+          <div key={i} className={`${styles.brawler_item}`}>
+            <h1 className={`${styles.brawler_name}`}>{item.name}</h1>
+            <img src={item.imageUrl} alt={item.name} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ app/page.jsx
@@ -0,0 +1,49 @@
+import styles from "./page.module.css"
+import HeroVideo from "./ui/page-components/HeroVideo/HeroVideo";
+import Container from "react-bootstrap/Container";
+import fonts from "./ui/fonts.css";
+import BrawlerSlider from "./ui/page-components/BrawlerSlider/BrawlerSlider";
+import Image from "next/image";
+
+
+export default function Home() {
+  return (
+    <>
+      <HeroVideo />
+      <div>
+      <Container fluid className={`${styles.Container}`}>
+        <BrawlerSlider />
+        <div className={`d-flex justify-content-start`}>
+          <Container>
+            <Image
+              alt="Phone and screenshot"
+              src="/media/edited-phone-image.png" /* image = https://wall.alphacoders.com/big.php?i=1279154 */
+              height={1200}
+              width={1200}
+              className={`${styles.phonepos} `}
+            />
+          </Container>
+          <Container className={`${styles.bodytext} mt-5 `}>
+            <h1> 
+              Your Ultimate Brawl Stars Tracker 🚀
+            </h1>
+            <h3>
+              <ul>
+              <li>🏆 Leaderboards at Your Fingertips: Stay ahead of the competition by tracking your progress and comparing stats with top players worldwide.</li>
+              <br />
+              <li>📊 In-Depth Player Stats: Dive into detailed statistics for each brawler, from win rates to power levels – empowering you to refine your strategies.</li>
+              <br />
+              <li>🌟 Event Notifications: Never miss a critical event again! Receive instant notifications for upcoming events, challenges, and special promotions.</li>
+              </ul>
+            </h3>
+         
+          </Container>
+        </div>
+      </Container>
+      </div>
+
+    </>
+  );
+}
+
+  
