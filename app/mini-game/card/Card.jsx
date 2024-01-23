52dba585cb0b5a63b1c4a51273728d5338016ced
GalenLovesCheese <galentay888@gmail.com>
2024-01-23 10:30:18 +0800

    Revert "Fixed Bugs: Added local storage for card faces"
    This reverts commit b10647920f85cdde26949e77a90e360ea53f5460.

--- app/mini-game/card/Card.jsx
+++ app/mini-game/card/Card.jsx
@@ -26,7 +26,8 @@ export default function Card({ card, handleChoice, flipped, disabled }) {
                         height={180}
                         width={180}
                         className={`${styles.cardImg} ${styles.back}`}
-                        onClick={handleClick}   
+                        onClick={handleClick}
+                        
                     />
                 </div>
             </div>
