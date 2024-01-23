b11c090bfd54d1048cadec23d22cb266c4d31ee0
Kilo <freddythr1247@gmail.com>
2024-01-23 09:59:47 +0800

    Testing

--- app/maps/page.jsx
+++ app/maps/page.jsx
@@ -4,6 +4,7 @@
 import React from "react";
 import useSWR from "swr";
 import styles from "./page.module.css";
+import Image from "next/image";
 
 function Banner() {
   return (
@@ -11,37 +12,10 @@ function Banner() {
           <img src="/media/Background.jpg"></img>
           <div className={`${styles.bannertext}`}>
               <div className={`${styles.whitebox}`}>
-                  <h1>Events</h1>
+                  <h1>Maps</h1>
                   <p>Navigate your way to victory!</p>
               </div>
           </div>
       </div>
   )
-}
-
-const fetcher = (...args) => fetch(...args).then((res) => res.json());
-
-export default function App1() {
-  const IP_API_URL = "https://api.brawlapi.com/v1/gamemodes";
-  const { data, error, isLoading } = useSWR(IP_API_URL, fetcher);
-
-  if (error) {
-    return <h1>failed to load</h1>;
-  }
-  if (isLoading) {
-    return <h1 className="loading">loading...</h1>;
-  }
-
-  return (
-    <div className={`${styles.Container}`}>
-      <div className={`${styles.brawlers_container}`}>
-        {data.list.map((item, i) => (
-          <div key={i} className={`${styles.brawler_item}`}>
-            <h1 className={`${styles.brawler_name}`}>{item.name}</h1>
-            <img src={item.imageUrl} alt={item.name} />
-          </div>
-        ))}
-      </div>
-    </div>
-  );
 }
