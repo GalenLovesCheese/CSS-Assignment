5aad11976462d4e334f66bf9b2b384af03ad6aec
mchljt <s10257089@connect.np.edu.sg>
2024-01-23 10:06:53 +0800

    before pulling galen's stuff

--- app/brawlers/page.jsx
+++ app/brawlers/page.jsx
@@ -3,7 +3,8 @@
 "use client";
 
 import React from "react";
-import fonts from "../ui/fonts.css"
+import fonts from "../ui/fonts.css";
+import { Modal } from "react-bootstrap";
 import useSWR from "swr";
 import styles from "./page.module.css";
 
@@ -13,6 +14,17 @@ const fetcher = (...args) => fetch(...args).then((res) => res.json());
 export default function App1() {
   const IP_API_URL = "https://api.brawlapi.com/v1/brawlers";
   const { data, error, isLoading } = useSWR(IP_API_URL, fetcher);
+
+    const [showModal, setShowModal] = React.useState(false);
+
+    /*Keep track of the brawler 
+    that was clicked on and is currently selected*/
+    const [selectedBrawler, setSelectedBrawler] = React.useState(null); 
+  
+    const handleBrawlerItemClick = (brawler) => {
+      setSelectedBrawler(brawler);
+      setShowModal(true);
+    };
 
   if (error) {
     return <h1>failed to load</h1>;
@@ -25,12 +37,41 @@ export default function App1() {
     <div className={`${styles.Container}`}>
       <div className={`${styles.brawlers_container}`}>
         {data.list.map((item, i) => (
-          <div key={i} className={`${styles.brawler_item}`}>
+          <div key={i} className={`${styles.brawler_item}`} onClick = {() => handleBrawlerItemClick(item)}>
             <h1 className={`${styles.brawler_name}`}>{item.name}</h1>
             <img src={item.imageUrl} alt={item.name} />
           </div>
         ))}
+        <Modal show={showModal} onHide={() => setShowModal(false)}>
+            <Modal.Header closeButton>
+              <Modal.Title className={`${styles.brawlerinfo_title}`}>{selectedBrawler ? selectedBrawler.name: ""}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body className = {`${styles.brawlerinfo}`} >
+              <img src = {selectedBrawler ? selectedBrawler.imageUrl: ""}/>
+              <h1 className={`${styles.brawlerinfo_header}`}>Description: </h1>
+              <h5>{selectedBrawler ? selectedBrawler.description: ""}</h5>
+              <h1 className={`${styles.brawlerinfo_header}`}>Star Powers: </h1>
+              {selectedBrawler.starPowers.map((item, i) => (
+              <div key={i} className={`${styles.StarPowersAndGadgets}`}>
+                <h2>{item.name}</h2>
+                <img className={`${styles.StarPowerandGadgetImages}`}src={item.imageUrl} alt={item.name} />
+                <h5>{item.description}</h5>
+              </div>
+              ))}
+              <h1 className={`${styles.brawlerinfo_header}`}>Gadgets: </h1> 
+              {selectedBrawler.gadgets.map((item, i) => (
+              <div key={i} className={`${styles.StarPowersAndGadgets}`}>
+                <h2>{item.name}</h2>
+                <img className={`${styles.StarPowerandGadgetImages}`}src={item.imageUrl} alt={item.name} />
+                <h5>{item.description}</h5>
+              </div>
+              ))}
+            </Modal.Body>
+        </Modal>
       </div>
     </div>
   );
 }
+
+
+
